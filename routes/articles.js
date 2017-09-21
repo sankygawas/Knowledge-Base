6720380b6ad9bdb8c20ae6733c1afce4bf0a31dc
Sanket Arun Gawas <sanketgawas1993@gmail.com>
2017-09-21 12:10:35 +0530

    Implementing route for getArticleById

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var Article = require('../models/article');
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   Article.getArticles(function(err,articles){
     if(err)
       console.log('error in retrieving')
     res.json(articles);
+  })
+});
+
+
+router.get('/:id', function(req, res, next) {
+  Article.getArticlesById(req.params.id, function(err,article){
+    if(err)
+      console.log('error in retrieving')
+    res.json(article);
   })
 });
 
